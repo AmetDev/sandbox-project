805ff15d0fd669e434d88e7838974c9085dfc08a
AmetDev <ametsejdaliev@gmail.com>
2023-06-24 15:48:27 +0300

    DASHA

---
+++ EXAM/EXAM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {11,1,2,3,5,4,6,7,8,9};
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = arr[j];
+                    }
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
